aabe6bab9410b5b028491f1310aff19329d03f86
kareypowell <kareypowell@gmail.com>
2019-09-19 17:46:47 -0500

    Remove unused imports and declarations

--- lib/controllers/heroes_controller.dart
+++ lib/controllers/heroes_controller.dart
@@ -7,15 +7,6 @@ class HeroesController extends ResourceController {
 
   final ManagedContext context;
 
-  final _heroes = [
-    {'id': 11, 'name': 'Mr. Nice'},
-    {'id': 12, 'name': 'Narco'},
-    {'id': 13, 'name': 'Bombasto'},
-    {'id': 14, 'name': 'Celeritas'},
-    {'id': 15, 'name': 'Magneta'},
-    {'id': 16, 'name': 'Karey'},
-  ];
-
   @Operation.get()
   Future<Response> getAllHeroes({@Bind.query('name') String name}) async {
     final heroQuery = Query<Hero>(context);
